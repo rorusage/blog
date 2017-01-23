bf3c21aab414cc7ef149b9ef7059362222d1285d
rorusage <rorusage@gmail.com>
2017-01-23 17:51:08 +0800

    Update find_post method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.new(post_params)
 
     if @post.save
       redirect_to posts_path, notice: "新增文章成功！"
@@ -46,6 +46,6 @@ class PostsController < ApplicationController
   end
 
   def find_post
-    @post = Post.find(params[:id])
+    @post = current_user.posts.find(params[:id])
   end
 end

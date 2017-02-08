d5187cbfdb39e68e09fc688a765aa214cfbf1265
rorusage <rorusage@gmail.com>
2017-02-08 17:34:47 +0800

    Add the dependent relation in post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   belongs_to :blog_owner, class_name: "User", foreign_key: :user_id
-  has_many :comments
+  has_many :comments, :dependent => :destroy
 end

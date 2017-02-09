5888d674bc69bd66dc43defedcb33351f8ed076b
rorusage <rorusage@gmail.com>
2017-02-09 16:53:27 +0800

    Each post can show how many comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124093259) do
+ActiveRecord::Schema.define(version: 20170209084412) do
 
   create_table "account_owners", force: :cascade do |t|
     t.integer  "user_id"
@@ -37,9 +37,10 @@ ActiveRecord::Schema.define(version: 20170124093259) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.integer  "user_id"
+    t.integer  "comments_count", default: 0
   end
 
   create_table "users", force: :cascade do |t|
